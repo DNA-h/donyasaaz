3a91543abe17d7ae1e4bd63abd989c0db8ec9ae2
DNA.h <s.n.bahador@gmail.com>
2021-05-18 23:39:39 +0430

    -fixed setting.py

--- donyasaaz/settings.py
+++ donyasaaz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#6y-d!$%wj1)a&5j4c0ie8q45h*&@%f+bxf*!gf&_8nh$mwzje'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '193.26.14.183']
 # Application definition
@@ -196,7 +196,7 @@ LOGGING = {
         'default': {
             'level': 'INFO',
             'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': 'C:\\Users\\DNA.h\\PycharmProjects\\donyasaaz\\logging.log',
+            'filename': '/home/donyasaaz/logging.log',
             'when': 'D',
             'interval': 7,
             'backupCount': 5,

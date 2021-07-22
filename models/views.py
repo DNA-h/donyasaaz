fb981cfc83b93965850897078da8733ae000f1d1
DNA.h <s.n.bahador@gmail.com>
2021-07-22 19:12:25 +0430

    -reduced to 3

--- models/views.py
+++ models/views.py
@@ -336,7 +336,7 @@ def get_prices():
     links = list(links)
     import random
     random.shuffle(links)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as pool:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as pool:
         for i in range(0, len(links)):
             site = re.findall("//(.*?)/", links[(i + 0) % len(links)]['url'])
             if not site:

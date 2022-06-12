1dddd6516f114564095cf3b2c6e69bcb84679bc3
DNA-h <s.n.bahador@gmail.com>
2022-06-12 15:21:08 +0430

    -fixed path \ insread of \\

--- models/crawlers/www_pixel_ir.py
+++ models/crawlers/www_pixel_ir.py
@@ -16,8 +16,8 @@ def pixel(link, headers, site):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('log-level=3')
-        sys.path.append("C:\Users\USER\donyasaaz\chromedriver.exe")
-        driver = webdriver.Chrome(executable_path="C:\Users\USER\donyasaaz\chromedriver.exe",
+        sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
                                   options=chrome_options)
         driver.get(link.url)
         soup = BeautifulSoup(driver.page_source, "html.parser")

9e20ac5c669c0ed66bbc1cdb3cd6892ce668484b
DNA-h <s.n.bahador@gmail.com>
2022-06-12 21:01:47 +0430

    - commit urls.py

--- donyasaaz/urls.py
+++ donyasaaz/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from models.views import *
+from models.tests import run_tests
 
 urlpatterns = [
     path('admin/', admin.site.urls),

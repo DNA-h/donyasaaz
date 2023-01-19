4134740a5c94e36b88ad34f1f0ca0d0a289bb1a9
DNA-h <s.n.bahador@gmail.com>
2023-01-19 13:35:17 +0330

    updated torob

--- models/crawlers/torob_com.py
+++ models/crawlers/torob_com.py
@@ -9,12 +9,15 @@ from selenium.webdriver.chrome.options import Options
 
 def torob(link, headers, site):
     try:
+        print('start');
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('log-level=3')
+        print(0)
         sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
+        print('append')
         driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
                                   options=chrome_options)
         print(1)

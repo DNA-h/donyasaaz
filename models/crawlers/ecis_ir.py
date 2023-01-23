6f50ab6ac3187fbd67b3227d51eb137121b4319f
DNA-h <s.n.bahador@gmail.com>
2023-01-23 19:57:57 +0330

    - fixed ecis

--- models/crawlers/ecis_ir.py
+++ models/crawlers/ecis_ir.py
@@ -10,33 +10,36 @@ from selenium.webdriver.chrome.options import Options
 import sys
 
 def ecis(link, headers, site):
-    try:
-        chrome_options = Options()
-        # chrome_options.add_argument("--headless")
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
-        driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
-                                  options=chrome_options)
-        driver.get(link.url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-    except Exception as e:
-        logger = logging.getLogger(__name__)
-        logger.info('%s :  %s,', site, e)
-        return None
-
-    if soup.find("button", attrs={"class": "single_add_to_cart_button button alt"}):
-        div = soup.find("p", attrs={"class": "price"})
-        if div is None:
-            return -1
-        p = div.find_all("span", attrs={"class": "woocommerce-Price-amount amount"})
-        if len(p) == 0:
-            return -1
-        elif len(p) == 1:
-            a = re.sub(r',', '', p[0].text).strip()
-        else:
-            a = re.sub(r',', '', p[1].text).strip()
-        b = re.findall(r'\d+', a)
-        return int(b[0])
-    else:
-        return -1
+    # try:
+    #     chrome_options = Options()
+    #     # chrome_options.add_argument("--headless")
+    #     chrome_options.add_argument('--no-sandbox')
+    #     chrome_options.add_argument('--disable-dev-shm-usage')
+    #     sys.path.append("C:\\Users\\DNA\\PycharmProjects\\donyasaaz\\chromedriver.exe")
+    #     driver = webdriver.Chrome(executable_path="C:\\Users\\DNA\\PycharmProjects\\donyasaaz\\chromedriver.exe",
+    #                               options=chrome_options)
+    #     driver.get(link.url)
+    #     soup = BeautifulSoup(driver.page_source, "html.parser")
+    # except Exception as e:
+    #     logger = logging.getLogger(__name__)
+    #     logger.info('%s :  %s,', site, e)
+    #     return None
+    #
+    # if soup.find("button", attrs={"class": "single_add_to_cart_button button alt"}):
+    #     div = soup.find("p", attrs={"class": "price"})
+    #     if div is None:
+    #         return -1
+    #     p = div.find_all("span", attrs={"class": "woocommerce-Price-amount amount"})
+    #     if len(p) == 0:
+    #         return -1
+    #     elif len(p) == 1:
+    #         if p[0].text == "تماس بگیرید!":
+    #             return -1
+    #         a = re.sub(r',', '', p[0].text).strip()
+    #     else:
+    #         a = re.sub(r',', '', p[1].text).strip()
+    #     b = re.findall(r'\d+', a)
+    #     return int(b[0])
+    # else:
+    # کلا شده تماس بگیرید
+    return -1

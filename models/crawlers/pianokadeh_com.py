29a873c2296438dfba47927fc471bac0e727de2f
Ali <vanquishzrr12@gmail.com>
2023-06-04 03:47:31 +0430

    pianokadeh add

--- models/crawlers/pianokadeh_com.py
+++ models/crawlers/pianokadeh_com.py
@@ -1,33 +1,74 @@
 import re
 import logging
+import sys
 
+from selenium import webdriver
 import requests
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 
 
 def pianokadeh(link, headers, site):
     try:
-        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-        response = requests.get(link.url, headers=headers, verify=False)
-        soup = BeautifulSoup(response.text, "html.parser")
-    except Exception as e:
-        logger = logging.getLogger(__name__)
-        logger.info('%s :  %s,', site, e)
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
+                                  options=chrome_options)
+        driver.get(link.url)
 
-        return None
+        try:
+            elements = driver.find_elements(By.CSS_SELECTOR, '#wz-product-price')
+            for element in elements:
 
-    s= soup.find("div", attrs={"class": "wz-shop-product-out-stock"})
-    if s is None:
-        q = soup.find("span",attrs={"id": "wz-product-price"})
-        intab='۱۲۳۴۵۶۷۸۹۰١٢٣٤٥٦٧٨٩٠'
-        outtab='12345678901234567890'
-        translation_table = str.maketrans(intab, outtab)
-        output_text = q.text.translate(translation_table)
-        a = re.sub(r',', '', output_text).strip()
-        b = re.findall(r'\d+', a)
-        if len(b) == 0:
+                price_text = element.text.strip()
+                price_text = convert_to_english(price_text)
+                if price_text != "":
+                    price_text = int(price_text)
+                    driver.close()
+                    return price_text
+                else:
+                    driver.close()
+                    return -1
+            driver.close()
             return -1
-        return int(b[0])
-    else:
+        except NoSuchElementException:
+            driver.close()
+            return -1
+
+    except Exception as ee:
         return -1
+
+
+def convert_to_english(text):
+    persian_to_english = {
+        '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
+        '۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
+        # Add more mappings for other Persian characters if needed
+    }
+
+    converted_text = ''
+
+    for char in text:
+        if char in persian_to_english:
+            converted_text += persian_to_english[char]
+        else:
+            converted_text += char
+
+    # Remove non-numeric characters
+    converted_text = ''.join(c for c in converted_text if c.isdigit())
+
+    return converted_text
+
+#
+# class MyObject:
+#     def __init__(self, url):
+#         self.url = url
+#
+#
+# item = MyObject("https://pianokadeh.com/shop/%D9%BE%DB%8C%D8%A7%D9%86%D9%88-%D8%AF%DB%8C%D8%AC%DB%8C%D8%AA%D8%A7%D9%84/P1213-casio-ap470.html")
+# print(pianokadeh(item, None, None))

14109775547ecb919abc65b6e8ebf4b1945c01d3
Ali <vanquishzrr12@gmail.com>
2023-06-04 02:46:13 +0430

    chavosh add 110

--- models/crawlers/sotplus_ir.py
+++ models/crawlers/sotplus_ir.py
@@ -1,30 +1,68 @@
 import re
 import logging
+import sys
 
 import requests
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 
 
 def sotplus(link, headers, site):
     try:
-        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-        response = requests.get(link.url, headers=headers, verify=False)
-        soup = BeautifulSoup(response.text, "html.parser")
-    except Exception as e:
-        logger = logging.getLogger(__name__)
-        logger.info('%s :  %s,', site, e)
-        
-        return None
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
+                                  options=chrome_options)
+        driver.get(link.url)
+        try:
+            element = driver.find_element(By.CSS_SELECTOR, '.is_stuck')
+            first_strong = element.find_element(By.CSS_SELECTOR, ".woocommerce-Price-currencySymbol")
+            parent_element = first_strong.find_element(By.XPATH, "..")
 
-    if soup.find("button", attrs={"class": "single_add_to_cart_button button alt"}):
-        p = soup.find("p", attrs={"class": "price"})
-        if p.find("ins") is not None:
-            s = p.find("ins")
-        else:
-            s = p.find("bdi")
-        a = re.sub(r'\.', '', s.text).strip()
-        b = re.findall(r'\d+', a)
-        return int(b[0])
-    else:
+            # Get the HTML content of the parent element
+            html_content = parent_element.get_attribute('innerHTML')
+
+            # Extract the pure text using regular expressions
+            price_text = re.sub('<[^>]+>', '', html_content)
+
+            if price_text != "":
+                price_text = int(price_text)
+                driver.close()
+                return price_text
+            else:
+                driver.close()
+                return -1
+
+        except NoSuchElementException:
+            driver.close()
+            return -1
+    except Exception as e:
         return -1
+
+
+
+def convert_to_english(text):
+    persian_to_english = {
+        '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
+        '۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
+        # Add more mappings for other Persian characters if needed
+    }
+
+    converted_text = ''
+
+    for char in text:
+        if char in persian_to_english:
+            converted_text += persian_to_english[char]
+        else:
+            converted_text += char
+
+    # Remove non-numeric characters
+    converted_text = ''.join(c for c in converted_text if c.isdigit())
+
+    return converted_text

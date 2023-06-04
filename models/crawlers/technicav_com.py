97f1f2c5c8d2ebe7968c4b81b4ef73460a8ca9a6
Ali <vanquishzrr12@gmail.com>
2023-06-04 04:34:03 +0430

    technicav add

--- models/crawlers/technicav_com.py
+++ models/crawlers/technicav_com.py
@@ -1,43 +1,94 @@
 import re
 import logging
-import requests
-from urllib3.exceptions import InsecureRequestWarning
-import os
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 import sys
+
+from selenium import webdriver
+import requests
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from urllib3.exceptions import InsecureRequestWarning
+from bs4 import BeautifulSoup
 
 def technicav(link, headers, site):
     try:
         chrome_options = Options()
         # chrome_options.add_argument("--headless")
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument("--disable-gpu")
+
         sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
         driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
                                   options=chrome_options)
         driver.get(link.url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-    except Exception as e:
-        logger = logging.getLogger(__name__)
-        logger.info('%s :  %s,', site, e)
-        return None
+        driver.implicitly_wait(12)
+        try:
+            elements = driver.find_elements(By.CSS_SELECTOR, '.product-details .product-price .out-price')
+            if elements:
+                driver.close()
+                return -1
+            else:
+                elements = driver.find_elements(By.CSS_SELECTOR, '.product-details .product-price .new-price')
+                if elements:
+                    for element in elements:
+                        price_text = element.text.strip()
+                        price_text = convert_to_english(price_text)
+                        if price_text != "":
+                            price_text = int(price_text)
+                            driver.close()
+                            return price_text
+                        else:
+                            driver.close()
+                            return -1
 
-    if soup.find("button", attrs={"class": "single_add_to_cart_button button alt"}):
-        div = soup.find("p", attrs={"class": "price"})
-        if div is None:
+                    driver.close()
+                    return -1
+                else:
+                    elements = driver.find_elements(By.CSS_SELECTOR, '.product-details .product-price')
+                    for element in elements:
+
+                        price_text = element.text.strip()
+                        price_text = convert_to_english(price_text)
+                        if price_text != "":
+                            price_text = int(price_text)
+                            driver.close()
+                            return price_text
+                        else:
+                            driver.close()
+                            return -1
+                    driver.close()
+                    return -1
+        except NoSuchElementException:
+            driver.close()
             return -1
-        p = div.find_all("span", attrs={"class":"woocommerce-Price-amount amount"})
-        if len(p) == 0:
-            return -1
-        elif len(p) == 1:
-            a = re.sub(r',', '', p[0].text).strip()
-        else:
-            a = re.sub(r',', '', p[1].text).strip()
-        b = re.findall(r'\d+', a)
-        return int(b[0])
-    else:
+    except Exception as ee:
         return -1
+
+
+def convert_to_english(text):
+    persian_to_english = {
+        '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
+        '۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
+        # Add more mappings for other Persian characters if needed
+    }
+
+    converted_text = ''
+
+    for char in text:
+        if char in persian_to_english:
+            converted_text += persian_to_english[char]
+        else:
+            converted_text += char
+
+    # Remove non-numeric characters
+    converted_text = ''.join(c for c in converted_text if c.isdigit())
+
+    return converted_text
+
+
+# class MyObject:
+#     def __init__(self, url):
+#         self.url = url
+#
+#
+# item = MyObject("https://www.technicav.com/products/audio-interface/m-audio-air-1924/")
+# print(technicav(item, None, None))

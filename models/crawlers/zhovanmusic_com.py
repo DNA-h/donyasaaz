38938e26ee4ad02b3c0b48120cd19762f0f23ec4
Ali <vanquishzrr12@gmail.com>
2023-06-13 01:43:59 +0430

    zhovan commit

--- models/crawlers/zhovanmusic_com.py
+++ models/crawlers/zhovanmusic_com.py
@@ -1,28 +1,86 @@
 import re
 import logging
+import sys
 
 import requests
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 
 
 def zhovanmusic(link, headers, site):
     try:
-        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-        response = requests.get(link.url, headers=headers, verify=False)
-        soup = BeautifulSoup(response.text, "html.parser")
-    except Exception as e:
-        logger = logging.getLogger(__name__)
-        logger.info('%s :  %s,', site, e)
-        return None
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        # sys.path.append("C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe")
+        # driver = webdriver.Chrome(executable_path="C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe", options=chrome_options)
 
-    if soup.find("button", attrs={"class": "add-to-cart button m-0"}):
-        p = soup.find("div", attrs={"class": "product-price"})
-        if p is None:
+        sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
+                             options=chrome_options)
+
+
+        driver.get(link.url)
+
+        try:
+            elements = driver.find_elements(By.CSS_SELECTOR, ".add-to-cart")
+            for element in elements:
+                inside_text = element.text.strip()
+                if "اتمام موجودی" in inside_text:
+                    driver.close()
+                    return -1
+                else:
+                    product_price = driver.find_element(By.CSS_SELECTOR, '.product-price')
+                    if product_price:
+                        price_text = product_price.text.strip()
+                        price_text = convert_to_english(price_text)
+                        if price_text != "":
+                            price_text = int(price_text)
+                            driver.close()
+                            return price_text
+                        else:
+                            driver.close()
+                            return -1
+                    else:
+                        driver.close()
+                        return -1
+            driver.close()
             return -1
-        else:
-            a = re.sub(r',', '', p.text).strip()
-        b = re.findall(r'\d+', a)
-        return int(b[0])
-    else:
+        except NoSuchElementException:
+            driver.close()
+            return -1
+
+    except Exception as ee:
         return -1
+
+def convert_to_english(text):
+    persian_to_english = {
+        '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
+        '۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
+        # Add more mappings for other Persian characters if needed
+    }
+
+    converted_text = ''
+
+    for char in text:
+        if char in persian_to_english:
+            converted_text += persian_to_english[char]
+        else:
+            converted_text += char
+
+    # Remove non-numeric characters
+    converted_text = ''.join(c for c in converted_text if c.isdigit())
+
+    return converted_text
+
+# class MyObject:
+#     def __init__(self, url):
+#         self.url = url
+#
+#
+# item = MyObject("https://www.zhovanmusic.com/store/sell-musical-instruments/piano-sales/casio-ap-470?utm_medium=PPC&utm_source=Torob")
+# print(zhovanmusic(item, None, None))

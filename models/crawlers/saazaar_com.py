fa31ddf5181dbe53ad5958953329913a697f5c51
Ali <vanquishzrr12@gmail.com>
2023-06-25 01:45:12 +0330

    saazaar fix commit

--- models/crawlers/saazaar_com.py
+++ models/crawlers/saazaar_com.py
@@ -1,33 +1,99 @@
 import re
 import logging
+import sys
 
 import requests
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 
 
 def saazaar(link, headers, site):
     try:
-        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-        response = requests.get(link.url, headers=headers, verify=False)
-        soup = BeautifulSoup(response.text, "html.parser")
-    except Exception as e:
-        logger = logging.getLogger(__name__)
-        logger.info('%s :  %s,', site, e)
-        return None
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        # sys.path.append("C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe")
+        # driver = webdriver.Chrome(executable_path="C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe",options=chrome_options)
 
-    if soup.find("button", attrs={"class": "single_add_to_cart_button button alt"}):
-        div = soup.find("p", attrs={"class": "price"})
-        if div is None:
+        sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
+                             options=chrome_options)
+
+        driver.get(link.url)
+
+        # FIXED WOOCOMMERCE PRO
+        try:
+            elements = driver.find_elements(By.CSS_SELECTOR, "h1 ~ .price")
+            for element in elements:
+                ins = element.find_element(By.TAG_NAME, 'ins')
+                bdi = ins.find_element(By.TAG_NAME, 'bdi')
+                price_text = bdi.text.strip()
+                price_text = convert_to_english(price_text)
+                if price_text != "":
+                    price_text = int(price_text)
+                    driver.close()
+                    return price_text
+                else:
+                    driver.close()
+                    return -1
+            driver.close()
             return -1
-        p = div.find_all("span", attrs={"class":"woocommerce-Price-amount amount"})
-        if len(p) == 0:
-            return -1
-        elif len(p) == 1:
-            a = re.sub(r',', '', p[0].text).strip()
-        else:
-            a = re.sub(r',', '', p[1].text).strip()
-        b = re.findall(r'\d+', a)
-        return int(b[0])
-    else:
+        except NoSuchElementException:
+            try:
+                elements = driver.find_elements(By.CSS_SELECTOR, 'h1 ~ .price')
+                if elements:
+                    for element in elements:
+                        bdi = element.find_element(By.TAG_NAME, 'bdi')
+                        price_text = bdi.text.strip()
+
+                        price_text = convert_to_english(price_text)
+                        if price_text != "":
+                            price_text = int(price_text)
+                            driver.close()
+                            return price_text
+                        else:
+                            driver.close()
+                            return -1
+                    driver.close()
+                    return -1
+                else:
+                    driver.close()
+                    return -1
+            except NoSuchElementException as e:
+                driver.close()
+                return -1
+
+    except Exception as ee:
         return -1
+
+def convert_to_english(text):
+    persian_to_english = {
+        '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
+        '۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
+        # Add more mappings for other Persian characters if needed
+    }
+
+    converted_text = ''
+
+    for char in text:
+        if char in persian_to_english:
+            converted_text += persian_to_english[char]
+        else:
+            converted_text += char
+
+    # Remove non-numeric characters
+    converted_text = ''.join(c for c in converted_text if c.isdigit())
+
+    return converted_text
+
+# class MyObject:
+#     def __init__(self, url):
+#         self.url = url
+#
+#
+# item = MyObject("https://saazaar.com/product/yamaha-psr-sx600/")
+# print(saazaar(item, None, None))

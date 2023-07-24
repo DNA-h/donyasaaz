e9a5a09616c1c31623a7aaa339344589936de16a
Ali <vanquishzrr12@gmail.com>
2023-07-24 03:42:14 +0330

    bycheck fix commit

---
+++ models/crawlers/bycheck_ir.py
@@ -0,0 +1,81 @@
+import re
+import logging
+import sys
+
+import requests
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from urllib3.exceptions import InsecureRequestWarning
+from bs4 import BeautifulSoup
+
+
+def bycheck(link, headers, site):
+    try:
+        chrome_options = Options()
+        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--follow-redirects")
+
+        # sys.path.append("C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe")
+        # driver = webdriver.Chrome(executable_path="C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe",options=chrome_options)
+
+        sys.path.append("C:\\Users\\USER\\donyasaaz\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="C:\\Users\\USER\\donyasaaz\\chromedriver.exe",
+                                  options=chrome_options)
+
+        driver.get(link.url)
+
+        # FIXED WOOCOMMERCE
+        try:
+            elements = driver.find_elements(By.CSS_SELECTOR, '.section-title')
+            for element in elements:
+                span = element.find_element(By.CSS_SELECTOR, '.text-blue')
+                price_text = span.text.strip()
+                price_text = convert_to_english(price_text)
+                if price_text != "":
+                    price_text = int(price_text)
+                    driver.close()
+                    return price_text
+                else:
+                    driver.close()
+                    return -1
+            driver.close()
+            return -1
+        except NoSuchElementException:
+            driver.close()
+            return -1
+
+    except Exception as ee:
+        return -1
+
+
+def convert_to_english(text):
+    persian_to_english = {
+        '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
+        '۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
+        # Add more mappings for other Persian characters if needed
+    }
+
+    converted_text = ''
+
+    for char in text:
+        if char in persian_to_english:
+            converted_text += persian_to_english[char]
+        else:
+            converted_text += char
+
+    # Remove non-numeric characters
+    converted_text = ''.join(c for c in converted_text if c.isdigit())
+
+    return converted_text
+
+
+# class MyObject:
+#     def __init__(self, url):
+#         self.url = url
+#
+#
+# item = MyObject("https://bycheck.ir/Product/165/%D9%BE%DB%8C%D8%A7%D9%86%D9%88_%D8%AF%DB%8C%D8%AC%DB%8C%D8%AA%D8%A7%D9%84_KORG_LP-180-WH")
+# print(bycheck(item, None, None))

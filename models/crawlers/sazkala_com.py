44599be6243b950fb0ee090575d16fb98d08fff1
Ali <vanquishzrr12@gmail.com>
2023-11-06 22:51:30 +0330

    new chrome driver and sites

--- models/crawlers/sazkala_com.py
+++ models/crawlers/sazkala_com.py
@@ -15,66 +15,91 @@ import sys
 
 def sazkala(link, headers, site):
     try:
-        chrome_options = Options()
-        #chrome_options.add_argument("--headless")
-        #chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--blink-settings=imagesEnabled=false')
-        # sys.path.append("C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe")
-        # driver = webdriver.Chrome(executable_path="C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe",options=chrome_options)
-        sys.path.append("C:\\Users\\hamed\\donyasaaz\\chromedriver.exe")
-        driver = webdriver.Chrome(executable_path="C:\\Users\\hamed\\donyasaaz\\chromedriver.exe",
-                                  options=chrome_options)
-
-        driver.set_page_load_timeout(40)
-        driver.get(link.url)
-
-        # WOOCOMMERCE PRO PLUS
-        cart = driver.find_elements(By.CSS_SELECTOR, "form.cart .single_add_to_cart_button")
-        if cart:
-            elements = driver.find_elements(By.CSS_SELECTOR, '.price-wrp')
-            for element in elements:
-                try:
-                    # simple product
-                    first_ins = element.find_element(By.TAG_NAME, 'ins')
-                    if first_ins:
-                        first_strong = first_ins.find_element(By.TAG_NAME, 'bdi')
-                        price_text = first_strong.text.strip()
-                        price_text = convert_to_english(price_text)
-                        if price_text != "":
-                            price_text = int(price_text)
-                            driver.close()
-                            return price_text
-                        else:
-                            driver.close()
-                            return -1
-                    else:
-                        driver.close()
-                        return -1
-                except NoSuchElementException:
-                    # variation-product
-                    # <ins> element is not available
-                    first_strong = element.find_element(By.TAG_NAME, 'bdi')
-                    price_text = first_strong.text.strip()
-                    price_text = convert_to_english(price_text)
-                    if price_text != "":
-                        price_text = int(price_text)
-                        driver.close()
-                        return price_text
-                    else:
-                        driver.close()
-                        return -1
-
-            driver.close()
-            return -1
-        else:
-            driver.close()
-            return -1
-
+        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+        response = requests.get(link.url, headers=headers, verify=False)
+        response.encoding = 'utf8'
+        soup = BeautifulSoup(response.text, "html.parser")
     except Exception as e:
-        print(e)
         logger = logging.getLogger(__name__)
         logger.info('%s :  %s,', site, e)
         return None
+
+    if soup.find("button", attrs={"class": "single_add_to_cart_button button alt"}):
+        div = soup.find("div", attrs={"class": "price-wrp"})
+        if div is None:
+            return -1
+        p = div.find_all("span", attrs={"class": "woocommerce-Price-amount amount"})
+        if len(p) == 0:
+            return -1
+        elif len(p) == 1:
+            a = convert_to_english(p[0].text.strip())
+        else:
+            a = convert_to_english(p[1].text.strip())
+        b = a
+        return b
+    else:
+        return -1
+    # try:
+    #     chrome_options = Options()
+    #     #chrome_options.add_argument("--headless")
+    #     #chrome_options.add_argument('--no-sandbox')
+    #     chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+    #     # sys.path.append("C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe")
+    #     # driver = webdriver.Chrome(executable_path="C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe",options=chrome_options)
+    #     sys.path.append("C:\\Users\\hamed\\donyasaaz\\chromedriver.exe")
+    #     driver = webdriver.Chrome(executable_path="C:\\Users\\hamed\\donyasaaz\\chromedriver.exe",
+    #                               options=chrome_options)
+    #
+    #     driver.set_page_load_timeout(40)
+    #     driver.get(link.url)
+    #
+    #     # WOOCOMMERCE PRO PLUS
+    #     cart = driver.find_elements(By.CSS_SELECTOR, "form.cart .single_add_to_cart_button")
+    #     if cart:
+    #         elements = driver.find_elements(By.CSS_SELECTOR, '.price-wrp')
+    #         for element in elements:
+    #             try:
+    #                 # simple product
+    #                 first_ins = element.find_element(By.TAG_NAME, 'ins')
+    #                 if first_ins:
+    #                     first_strong = first_ins.find_element(By.TAG_NAME, 'bdi')
+    #                     price_text = first_strong.text.strip()
+    #                     price_text = convert_to_english(price_text)
+    #                     if price_text != "":
+    #                         price_text = int(price_text)
+    #                         driver.close()
+    #                         return price_text
+    #                     else:
+    #                         driver.close()
+    #                         return -1
+    #                 else:
+    #                     driver.close()
+    #                     return -1
+    #             except NoSuchElementException:
+    #                 # variation-product
+    #                 # <ins> element is not available
+    #                 first_strong = element.find_element(By.TAG_NAME, 'bdi')
+    #                 price_text = first_strong.text.strip()
+    #                 price_text = convert_to_english(price_text)
+    #                 if price_text != "":
+    #                     price_text = int(price_text)
+    #                     driver.close()
+    #                     return price_text
+    #                 else:
+    #                     driver.close()
+    #                     return -1
+    #
+    #         driver.close()
+    #         return -1
+    #     else:
+    #         driver.close()
+    #         return -1
+    #
+    # except Exception as e:
+    #     print(e)
+    #     logger = logging.getLogger(__name__)
+    #     logger.info('%s :  %s,', site, e)
+    #     return None
 
 
 def convert_to_english(text):
@@ -103,5 +128,8 @@ def convert_to_english(text):
 #         self.url = url
 #
 #
-# item = MyObject("https://sazkala.com/product/roland-rp30-digital-piano/")
+# item = MyObject("https://sazkala.com/product/yamaha-yh-e500a/")
+# print(sazkala(item, None, None))
+#
+# item = MyObject("https://sazkala.com/product/ltd-mt-130-orange/")
 # print(sazkala(item, None, None))

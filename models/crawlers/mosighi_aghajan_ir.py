36c75f3890f1141413b87e5b3ced21fc89ab435b
Ali <vanquishzrr12@gmail.com>
2023-12-13 01:51:06 +0330

    new chrome driver and sites

--- models/crawlers/mosighi_aghajan_ir.py
+++ models/crawlers/mosighi_aghajan_ir.py
@@ -11,7 +11,7 @@ from urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 
 
-def mosighi_aghajan_ir(link, headers, site):
+def mosighi_aghajan(link, headers, site):
     try:
         chrome_options = Options()
         # chrome_options.add_argument("--headless")

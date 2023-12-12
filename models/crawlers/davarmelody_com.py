455be50c3e3eef74728f105f1ab94dcf1a2c4a88
Ali <vanquishzrr12@gmail.com>
2023-12-12 21:59:46 +0330

    new chrome driver and sites

--- models/crawlers/davarmelody_com.py
+++ models/crawlers/davarmelody_com.py
@@ -1,31 +1,98 @@
 import re
 import logging
+import sys
 
 import requests
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from urllib3.exceptions import InsecureRequestWarning
 from bs4 import BeautifulSoup
 
 
+
 def davarmelody(link, headers, site):
     try:
-        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-        response = requests.get(link.url, headers=headers, verify=False)
-        soup = BeautifulSoup(response.text, "html.parser")
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+
+        # sys.path.append("C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe")
+        # driver = webdriver.Chrome(executable_path="C:\\MyBackups\\robot donyayesaaz\\chromedriver.exe",options=chrome_options)
+
+        sys.path.append("C:\\Users\\hamed\\donyasaaz\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="C:\\Users\\hamed\\donyasaaz\\chromedriver.exe",
+                                  options=chrome_options)
+
+        driver.set_page_load_timeout(40)
+        driver.get(link.url)
+
+        cart = driver.find_elements(By.CSS_SELECTOR, "#addToCart")
+        if cart:
+            elements = driver.find_elements(By.CSS_SELECTOR, '#product h2')
+            for element in elements:
+                try:
+                    # simple product
+                    first_ins = element.find_element(By.CSS_SELECTOR, 'span[itemprop="price"]')
+                    if first_ins:
+                        price_text = first_ins.text.strip()
+                        price_text = convert_to_english(price_text)
+                        if price_text != "":
+                            price_text = int(price_text)
+                            driver.close()
+                            return price_text
+                        else:
+                            driver.close()
+                            return -1
+                    else:
+                        driver.close()
+                        return -1
+                except NoSuchElementException:
+                    driver.close()
+                    return -1
+
+            driver.close()
+            return -1
+        else:
+            driver.close()
+            return -1
+
     except Exception as e:
+        print(e)
         logger = logging.getLogger(__name__)
         logger.info('%s :  %s,', site, e)
-        
         return None
 
-    if soup.find("div", attrs={"id": "product"}).find("p") is not None:
-        p = soup.find("span", attrs={"itemprop": "price"})
-        if p is None:
-            return -1
-        s = re.sub(r'\s+', ' ', p.text).strip()
-        a = re.sub(r',', '', s)
-        b = re.findall(r'\d+', a)
-        if len(b) == 0:
-            return -1
-        return int(b[0])
-    else:
-        return -1
+def convert_to_english(text):
+    persian_to_english = {
+        '۰': '0', '۱': '1', '۲': '2', '۳': '3', '۴': '4',
+        '۵': '5', '۶': '6', '۷': '7', '۸': '8', '۹': '9',
+        # Add more mappings for other Persian characters if needed
+    }
+
+    converted_text = ''
+
+    for char in text:
+        if char in persian_to_english:
+            converted_text += persian_to_english[char]
+        else:
+            converted_text += char
+
+    # Remove non-numeric characters
+    converted_text = ''.join(c for c in converted_text if c.isdigit())
+
+    return converted_text
+
+
+# class MyObject:
+#     def __init__(self, url):
+#         self.url = url
+#
+#
+# item = MyObject("https://davarmelody.com/boss-katana-50-mkii")
+# print(davarmelody(item, None, None))
+#
+# item = MyObject("https://davarmelody.com/solid-state-logic-the-bus-plus")
+# print(davarmelody(item, None, None))

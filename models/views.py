fe1598cbfd4f3c4f3975be29b3f7dc78eb1da40a
Ali <vanquishzrr12@gmail.com>
2024-05-12 20:39:49 +0330

    comment site priorities

--- models/views.py
+++ models/views.py
@@ -313,19 +313,19 @@ def create_and_download_backup():
     import requests
 
     # Specify the file to upload
-    file_path = 'C:\\Users\\hamed\\donyasaaz\\static\\dump.sql'
-    url = 'https://donyayesaaz.com/robot-backups/upload.php'
-
-    # Set your password
-    password = 'm@smzkaalxx1223ss'
-
-    # Open the file
-    with open(file_path, 'rb') as file:
-        # Send a POST request with password and file to the server
-        response = requests.post(url, files={'file': file}, data={'password': password})
-
-    # Print the server's response
-    print(response.text)
+    # file_path = 'C:\\Users\\hamed\\donyasaaz\\static\\dump.sql'
+    # url = 'https://donyayesaaz.com/robot-backups/upload.php'
+    #
+    # # Set your password
+    # password = 'm@smzkaalxx1223ss'
+    #
+    # # Open the file
+    # with open(file_path, 'rb') as file:
+    #     # Send a POST request with password and file to the server
+    #     response = requests.post(url, files={'file': file}, data={'password': password})
+    #
+    # # Print the server's response
+    # print(response.text)
 
 @csrf_exempt
 @api_view(['GET'])
